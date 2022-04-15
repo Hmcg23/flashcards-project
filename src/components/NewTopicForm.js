c71f71a8b5d19855cd79a49716e3626e392c0539
H_mcg <82006254+Hmcg23@users.noreply.github.com>
2022-04-15 15:49:09 -0700

    add topic creation functionality

--- src/components/NewTopicForm.js
+++ src/components/NewTopicForm.js
@@ -3,11 +3,14 @@ import { useHistory } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import ROUTES from "../app/routes";
 import { ALL_ICONS } from "../data/icons";
+import { addTopic } from "../features/topics/topicsSlice";
+import { useDispatch } from "react-redux";
 
 export default function NewTopicForm() {
   const [name, setName] = useState("");
   const [icon, setIcon] = useState("");
   const history = useHistory();
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -16,6 +19,8 @@ export default function NewTopicForm() {
     }
 
     // dispatch your add topic action here
+    dispatch(addTopic({ id: uuidv4(), name: name, icon: icon}));
+
     history.push(ROUTES.topicsRoute());
   };
 
