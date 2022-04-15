ab312d71713c58932919e944a5d939faebaf90ed
H_mcg <82006254+Hmcg23@users.noreply.github.com>
2022-04-15 10:43:23 -0700

    add topicsReducer to store

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -7,17 +7,66 @@ const topicsSlice = createSlice({
     },
     reducers: {
         addTopic: (state, action) => {
-            const { id, name, icon, quizIds } = action.payload;
-            state.topics[quizIds] = {
+            const { id, name, icon } = action.payload;
+            state.topics[id] = {
                 id: id,
                 name: name,
-                icon: icon
+                icon: icon,
+                quizIds: []
             };
         }
     }
-})
+});
 
 // selector
 export const selectTopics = state => state.topics.topics;
 
 export default topicsSlice.reducer;
+
+export const { addTopic } = topicsSlice.actions;
+
+/* Example State
+
+{
+  topics: {
+    topics: {
+      '123': {
+        id: '123',
+        name: 'example topic',
+        icon: 'icon url',
+        quizIds: ['456']
+      }
+    }
+  },
+  quizzes: {
+    quizzes: {
+      '456': {
+        id: '456',
+        topicId: '123',
+        name: 'quiz for example topic',
+        cardIds: ['789', '101', '102']
+      }
+    }
+  },
+  cards: {
+    cards: {
+      '789': {
+        id: '789',
+        front: 'front text',
+        back: 'back text'
+      },
+      '101': {
+        id: '101',
+        front: 'front text',
+        back: 'back text'
+      },
+      '102': {
+        id: '102',
+        front: 'front text',
+        back: 'back text'
+      },
+    }
+  }
+}
+
+*/
